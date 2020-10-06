790aaea1ef80712817cded28800bfdd5917942cb
mitchdobry <mitch.dobry.jas@gmail.com>
2020-10-06 10:25:28 +0200

    Updates

---
+++ precache-manifest.11943aa73cf1effd86fd44ca316eed1f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5925b7151d87bc4828aaed35820ef5d",
+    "url": "/ABCweb/index.html"
+  },
+  {
+    "revision": "08eed80aec60cc2bf0d2",
+    "url": "/ABCweb/static/css/main.b7c9f561.chunk.css"
+  },
+  {
+    "revision": "0056169227e78ea70fe6",
+    "url": "/ABCweb/static/js/2.9d5a308b.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/ABCweb/static/js/2.9d5a308b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08eed80aec60cc2bf0d2",
+    "url": "/ABCweb/static/js/main.c9a3d06f.chunk.js"
+  },
+  {
+    "revision": "0e8a86ec77a01ed16292",
+    "url": "/ABCweb/static/js/runtime-main.5fb16a76.js"
+  },
+  {
+    "revision": "c17e66e01f8c5a1063c9e270c6798988",
+    "url": "/ABCweb/static/media/Olaciesielska.c17e66e0.jpg"
+  },
+  {
+    "revision": "b219b32dfe01557f4504c46d90d3dddd",
+    "url": "/ABCweb/static/media/agnieszka.b219b32d.jpg"
+  },
+  {
+    "revision": "8883378adce92dfef829a1bb7a287424",
+    "url": "/ABCweb/static/media/facebook-f-brands.8883378a.svg"
+  },
+  {
+    "revision": "2a3c056cccb82501c5dcbeb2c287c096",
+    "url": "/ABCweb/static/media/integracjasenso.2a3c056c.jpg"
+  },
+  {
+    "revision": "d42598053a172ddfc971dd5270aae0e3",
+    "url": "/ABCweb/static/media/local.d4259805.jpg"
+  },
+  {
+    "revision": "0929a958d94e134ee996f0aacdd65f5f",
+    "url": "/ABCweb/static/media/logo.0929a958.svg"
+  },
+  {
+    "revision": "1a5179ad6b32e59e742df8a8f2da6342",
+    "url": "/ABCweb/static/media/logo2.1a5179ad.svg"
+  },
+  {
+    "revision": "f85efaf7b7883d5bd9361a1c20558482",
+    "url": "/ABCweb/static/media/marta.f85efaf7.jpg"
+  },
+  {
+    "revision": "a89599cc761ebcfb3221b26590ccbdbc",
+    "url": "/ABCweb/static/media/martf.a89599cc.jpg"
+  },
+  {
+    "revision": "0a2c39c806e13905bed8a327550f8715",
+    "url": "/ABCweb/static/media/reka.0a2c39c8.jpg"
+  },
+  {
+    "revision": "9aa089e6a0012ec72562851bb8173f28",
+    "url": "/ABCweb/static/media/sensoplastyka.9aa089e6.jpg"
+  },
+  {
+    "revision": "918529db42b03255c4732e54f5350bb4",
+    "url": "/ABCweb/static/media/suit.918529db.jpg"
+  }
+]);
